6e949ecc39a05e375bc5947a4223817a3dace4f6
JuanNunezP <87092987+JuanNunezP@users.noreply.github.com>
2021-07-07 13:55:23 -0500

    Create ejercicios.js

---
+++ ejercicios.js
@@ -0,0 +1,35 @@
+
+function cadena(){
+    let palabra="Hola Juan";
+    console.log(palabra.length);
+}
+
+cadena();
+
+
+function recortar() {
+    let palabrota = 'Hola Mundo';
+    console.log(palabrota.slice(0, 4));
+}
+
+recortar();
+
+function repetir(palabrita,veces) {
+
+    console.log(palabrita.repeat(veces))
+    
+}
+
+repetir("Hola Mundo ",4);
+
+
+function array() {
+     
+
+    let cadena = "Hola Que Tal Perras";
+
+    console.log(cadena.split(" "));
+
+}
+
+array();
